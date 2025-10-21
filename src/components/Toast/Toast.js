7734838ff85111ef79b035ca2c50f28c2f7d920c
zsigmund-od <zsigmund@overdrive.com>
2025-10-21 16:53:57 -0400

    5.2 Screen reader users

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -26,10 +26,17 @@ function Toast({ children, variant, onClose }) {
       <div className={styles.iconContainer}>
         <Icon size={24} />
       </div>
-      <p className={styles.content}>{children}</p>
-      <button className={styles.closeButton} onClick={() => onClose?.()}>
+      <p className={styles.content}>
+        <VisuallyHidden>{variant} - </VisuallyHidden>
+        {children}
+      </p>
+      <button
+        className={styles.closeButton}
+        onClick={() => onClose?.()}
+        aria-label="Dismiss message"
+        aria-live="off"
+      >
         <X size={24} />
-        <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
     </div>
   );
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -20,7 +20,12 @@ function ToastShelf() {
   });
 
   return (
-    <ol className={styles.wrapper}>
+    <ol
+      className={styles.wrapper}
+      role="region"
+      aria-live="polite"
+      aria-label="Notification"
+    >
       {toasts.map(({ id, message, variant }) => (
         <li key={id} className={styles.toastWrapper}>
           <Toast variant={variant} onClose={() => removeToast(id)}>
